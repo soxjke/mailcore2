f44fb8fc0fcb16028fb95dfd0b5d2eaa578463a6
Petro Korienev <soxjke@gmail.com>
2013-08-08 17:48:25 +0300

    fix to MCAssertInternal - now it exists only in DEBUG mode

--- src/core/basetypes/MCAssert.c
+++ src/core/basetypes/MCAssert.c
@@ -5,10 +5,12 @@
 
 void MCAssertInternal(const char * filename, unsigned int line, int cond, const char * condString)
 {
+#ifdef DEBUG
     if (cond) {
         return;
     }
     
     fprintf(stderr, "%s:%i: assert %s\n", filename, line, condString);
     abort();
+#endif
 }
